8bffbdf06ef80477163c82954b45524e87b57ffc
Gabriele Di Egidio <diegidiogabriele@gmail.com>
2022-06-22 17:07:29 +0200

    Fix

--- procedura_1.sql
+++ procedura_1.sql
@@ -3,17 +3,14 @@ DROP PROCEDURE IF EXISTS `guida_tv`.`signUp`;
 DELIMITER $
 
 -- 1 > [Registrazione (inserimento dei dati del profilo) di un utente.] < --
-CREATE PROCEDURE `signUp`(IN email_param VARCHAR(64), IN pwd_param VARCHAR(32), IN fascia_email_param ENUM ('Mattina', 'Pomeriggio', 'Sera') )
+CREATE PROCEDURE `signUp`(IN email_param VARCHAR(64), IN pwd_param VARCHAR(32), IN fascia_email_param ENUM ('Nessuno','Mattina', 'Pomeriggio', 'Sera'))
 BEGIN
-
 		IF (SELECT email_param NOT REGEXP '[a-zA-Z0-9._-].*@[a-zA-Z0-9._-].*\\.[a-zA-Z0-9].')
 			THEN
 				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'email inserita non corretta';
 			ELSE
 				INSERT INTO `guida_tv`.utente(`email`, `pwd`, `orario_email`) VALUES (email_param, pwd_param, fascia_email_param);
-	END IF;
-        
+		END IF;
 END $
 
 DELIMITER $
-
