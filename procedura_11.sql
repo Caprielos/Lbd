7e3138f308dd8fbfd539cf5eb096400d7abe584a
Gabriele Di Egidio <diegidiogabriele@gmail.com>
2022-06-28 16:41:33 +0200

    bla bla bla

--- procedura_11.sql
+++ procedura_11.sql
@@ -42,8 +42,7 @@ BEGIN
 		JOIN palinsesto pal ON prog.id = pal.id_programma
 		JOIN canale_preferito can_pref ON pal.id_canale = can_pref.id_canale
 		LEFT JOIN canale can ON pal.id_canale = can.id
-		WHERE pal.ora_inizio >= ora_inizio AND pal.ora_fine <= ora_fine AND can_pref.id_utente = id_param AND prog_pref.id_utente = id_param 
-        -- AND pal.giorno = curdate()
+		WHERE pal.ora_inizio >= ora_inizio AND pal.ora_fine <= ora_fine AND can_pref.id_utente = id_param AND prog_pref.id_utente = id_param
         ORDER BY pal.ora_inizio ASC;
         
 		SET email_result = "Le ricordiamo che oggi al canale: ";
